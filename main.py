088bf5c32cf26524ad9b5d4d04764b9b8b884818
Dillow <madushanka20021224@gmail.com>
2026-02-25 18:45:24 +0530

    Matplotlib to use a non-GUI backend

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import matplotlib
+matplotlib.use('Agg')  
+
 from flask import Flask, render_template, request
 import hashlib
 import bcrypt
@@ -8,7 +11,6 @@ import json
 import base64
 from io import BytesIO
 import matplotlib.pyplot as plt
-
 app = Flask(__name__)
 
 
